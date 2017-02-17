1c1d46e1d772918b5cac210d64c92e5ffe0e9420
Andrew Woods <atwoods1@gmail.com>
2017-02-17 01:34:15 -0800

    Add methods to getStates and isPostalCodeValid
    Create new methods and their associated tests.

--- tests/UnitedStatesTest.php
+++ tests/UnitedStatesTest.php
@@ -18,6 +18,54 @@ class UnitedStatesTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($expected, $us->getFullName());
 	}
 
+	public function testGetStates(){
+		$expectedTotal = 50;
+		$includeDC = false;
 
+		$us = new \Awoods\World\UnitedStates();
+
+		$this->assertEquals($expectedTotal, count($us->getStates($includeDC)));
+	}
+
+	public function testGetStatesIncludeDC(){
+		$expectedTotal = 51;
+
+		$includeDC = true;
+		$us = new \Awoods\World\UnitedStates();
+
+		$this->assertEquals($expectedTotal, count($us->getStates($includeDC)));
+	}
+
+	public function testIsPostalCodeValid(){
+		$us = new \Awoods\World\UnitedStates();
+
+		$goodSeattleZipcode = '98109';
+		$this->assertTrue($us->isPostalCodeValid($goodSeattleZipcode));
+
+
+		$goodSeattleZipcodePlusFour = '98109-1234';
+		$this->assertTrue($us->isPostalCodeValid($goodSeattleZipcodePlusFour));
+
+		$goodSchenectadyZipcode = '12345';
+		$this->assertTrue($us->isPostalCodeValid($goodSchenectadyZipcode));
+
+		$badZipcode = '12345-1';
+		$this->assertFalse($us->isPostalCodeValid($badZipcode));
+
+		$badZipcode = '12345-12';
+		$this->assertFalse($us->isPostalCodeValid($badZipcode));
+
+		$badZipcode = '12345-123';
+		$this->assertFalse($us->isPostalCodeValid($badZipcode));
+
+		// Valid Postal code for Vancouver, BC, Canada
+		$badCanadianZipcode = 'V5K 0A1';
+		$this->assertFalse($us->isPostalCodeValid($badCanadianZipcode));
+
+		$goodBeverlyHillsZipcode = '90210';
+		$this->assertTrue($us->isPostalCodeValid($goodBeverlyHillsZipcode));
+
+	}
 
 }
+
