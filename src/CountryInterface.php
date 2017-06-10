2a377431364e44e9f44b8651d95d57789881a9d4
Andrew Woods <atwoods1@gmail.com>
2017-06-10 10:56:41 -0700

    Remove validation methods from Country Interface
    Not all countries use postal codes.

--- src/CountryInterface.php
+++ src/CountryInterface.php
@@ -16,21 +16,5 @@ interface CountryInterface {
 	 */
 	public function getFullName();
 
-	/**
-	 * @param string $postalCode
-	 *
-	 * @return bool
-	 */
-	public function isPostalCodeValid($postalCode);
-
-	/**
-	 * Determine if the provided phone number looks correct.
-	 *
-	 * @param string $phoneNumber
-	 * @param bool $strict Optional. Default is false. Do you want interpret the value according to the country standard?
-	 *
-	 * @return bool
-	 */
-	public function isPhoneNumberValid($phoneNumber, $strict = false);
 }
 
