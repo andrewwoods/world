006672b8ee752079e9ef548ab3e1ee9872d576fc
Andrew Woods <atwoods1@gmail.com>
2017-08-25 18:00:32 -0700

    Add Continent class to project

---
+++ src/Continent.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This is part of the World project.
+ */
+
+namespace Awoods\World;
+
+/**
+ * Class Continent
+ * @package Awoods\World
+ */
+class Continent
+{
+    /** @var string */
+    protected $abbr;
+
+    /** @var  string */
+    protected $name;
+
+    /** @var array */
+    protected $countries = [];
+
+    /**
+     * Continent constructor.
+     *
+     * @param $abbr string Two-letter abbreviation
+     * @param $name string Full name of the continent
+     */
+    public function __construct($abbr, $name)
+    {
+        $this->abbr = $abbr;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAbbr() : string
+    {
+        return $this->abbr;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param CountryInterface $country
+     *
+     * @return void
+     */
+    public function addCountry(CountryInterface $country) : void
+    {
+        $this->countries[] = $country;
+    }
+
+    /**
+     * @return CountryInterface[]
+     */
+    public function getCountries()
+    {
+        return $this->countries;
+    }
+}
