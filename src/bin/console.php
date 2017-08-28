247b4288973d0b464093160bb7d265e3275bdb08
Andrew Woods <atwoods1@gmail.com>
2017-08-27 19:34:03 -0700

    Update console command
    Describe what you did and why

--- src/bin/console.php
+++ src/bin/console.php
@@ -1,18 +1,24 @@
 #!/usr/bin/env php
 <?php
 /**
- * The World project
+ * This is part of the World project.
+ *
+ * @license https://opensource.org/licenses/mit-license.php MIT
  */
+
 $projectPath = dirname(dirname(__FILE__ ));
 require_once $projectPath . '/vendor/autoload.php';
 require_once $projectPath . '/ConsoleCommands/CountryCreateCommand.php';
+require_once $projectPath . '/ConsoleCommands/CountryLoaderCommand.php';
 
 use Awoods\World\ConsoleCommands\CountryCreateCommand;
+use Awoods\World\ConsoleCommands\CountryLoaderCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application('World Country Creation Tool');
 
 // Register your commands
 $application->add(new CountryCreateCommand());
+$application->add(new CountryLoaderCommand());
 
 $application->run();
