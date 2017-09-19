2e3bcd939640b09c3bf71cbc685543624e7596b2
Andrew Woods <atwoods1@gmail.com>
2017-09-19 16:58:59 -0700

    Clean up console application
    Remove obsolete Country Create command.
    Simplify path to autoload script.

--- bin/console.php
+++ bin/console.php
@@ -6,18 +6,14 @@
  * @license https://opensource.org/licenses/mit-license.php MIT
  */
 
-$projectPath = dirname(dirname(__FILE__ ));
+require_once '../vendor/autoload.php';
 
-require_once $projectPath . '/vendor/autoload.php';
-
-use Awoods\World\ConsoleCommands\CountryCreateCommand;
 use Awoods\World\ConsoleCommands\CountryLoaderCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application('World Application');
 
 // Register your commands
-$application->add(new CountryCreateCommand());
 $application->add(new CountryLoaderCommand());
 
 $application->run();
