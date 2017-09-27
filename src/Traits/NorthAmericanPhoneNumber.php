33e4948f4d9788ab868bb3417f8679028aac6ca5
Andrew Woods <atwoods1@gmail.com>
2017-09-27 12:30:04 -0700

    Simplify logic for strict phone number validation
    Reduce complexity to make it easier to understand.

--- src/Traits/NorthAmericanPhoneNumber.php
+++ src/Traits/NorthAmericanPhoneNumber.php
@@ -36,32 +36,35 @@ trait NorthAmericanPhoneNumber {
 			$strictRegex = '/^\(?([2–9][0-9][0-9])\)?(?:[-\. ])?([2-9][0-9][0-9])(?:[-\. ])?(\d{4})$/';
 			preg_match($strictRegex, $phoneNumber, $matches);
 
-			if (isset($matches[0]) && $matches[0] === $phoneNumber){
+            if (empty($matches[0])) {
+                return false;
+            }
 
-				$area   = $matches[1];
-				$prefix = $matches[2];
-				$code   = $matches[3];
+            if ($matches[0] !== $phoneNumber) {
+                return false;
+            }
 
-				// the numbers X11 are reserved e.g. 411, 611
-				// These 8 ERCs, called service codes, are not used as area codes.
-				if ('11' === substr($area,1, 2)){
-					return false;
-				}
+            $area = $matches[1];
+            $prefix = $matches[2];
+            $code = $matches[3];
 
-				// N9X - The 80 codes in this format, called expansion codes, have been reserved for use during the period when the current 10-digit NANP number format undergoes expansion.
-				if ('9' === substr($area, 1, 1)){
-					return false;
-				}
+            // the numbers X11 are reserved e.g. 411, 611
+            // These 8 ERCs, called service codes, are not used as area codes.
+            if ('11' === substr($area, 1, 2)) {
+                return false;
+            }
 
-				// 37X and 96X - Two blocks of 10 codes each have been set aside by the INC for unanticipated purposes where it may be important to have a full range of 10 contiguous codes available.
-				if ('37' === substr($area, 0, 2) || '96' === substr($area, 0, 2)){
-					return false;
-				}
+            // N9X - The 80 codes in this format, called expansion codes, have been reserved for use during the period when the current 10-digit NANP number format undergoes expansion.
+            if ('9' === substr($area, 1, 1)) {
+                return false;
+            }
 
-				return true;
-			}
+            // 37X and 96X - Two blocks of 10 codes each have been set aside by the INC for unanticipated purposes where it may be important to have a full range of 10 contiguous codes available.
+            if ('37' === substr($area, 0, 2) || '96' === substr($area, 0, 2)) {
+                return false;
+            }
 
-			return false;
+            return true;
 
 		} else {
 			$simpleRegex = '/^(\+?1? ?)?((\(\d{3}\)|\d{3}))([-\. ])?\d{3}([-\. ])?\d{4}$/';
